b3c6c6d3c01766355067ebad066a34145df6d00d
vm <vm@Unknown>
2021-10-11 09:15:49 +0200

    picanha

--- src/picanhaburger/PicanhaBurger.java
+++ src/picanhaburger/PicanhaBurger.java
@@ -1,8 +1,51 @@
 package picanhaburger;
 
+import java.io.*;
+
 public class PicanhaBurger {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        
+        byte op;
+        
+        do {
+            System.out.println("1. Receber os dados da Venda");
+            System.out.println("2. Visualizar o valor a pagar");
+            System.out.println("3. Dados da venda");
+            System.out.println("4. Valor da total recebido pela empresa");
+            System.out.println("5. Quantidade de pedidos para cada tipo de hamburger");
+            System.out.println("6. Visualizar o ingrediente mais solicitadoo");
+            System.out.println("0. Sair");
+            do {
+                System.out.print("Opcao: ");
+                op = Byte.parseByte(rd.readLine());
+                if( op < 0 || op > 6)
+                    System.out.println("Opcao ivalida");
+            }while(op < 0 || op > 6);
+            
+            switch(op) {
+                case 1:
+                
+                break;
+                case 2:
+                
+                break;
+                case 3:
+                
+                break;
+                case 4:
+                
+                break;
+                case 5:
+                
+                break;
+                case 6:
+                
+                break;
+            }
+            
+        }while(op != 0);
         
     }
     
