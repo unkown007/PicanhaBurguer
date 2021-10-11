32882354aeb52766c173bcf42b50aca9c5aea6ad
vm <vm@Unknown>
2021-10-11 09:51:28 +0200

    estrutura de dados

--- src/picanhaburger/PicanhaBurger.java
+++ src/picanhaburger/PicanhaBurger.java
@@ -6,8 +6,35 @@ public class PicanhaBurger {
 
     public static void main(String[] args) throws IOException {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        
         byte op;
+        
+        final byte SIMPLES  = (byte) 120;
+        final byte COMPLETO = (byte) 150;
+        
+        // ingredientes extras
+        final byte OVO = 50;
+        final byte BATATA = 70;
+        final byte FIAMBRE = 30;
+        
+        int qtdOvo;
+        int qtdBatata;
+        int qtdFiambre;
+        
+        qtdOvo = qtdBatata = qtdFiambre = 0;
+        
+        int qtdSimples;
+        int qtdCompleto;
+        
+        qtdSimples = qtdCompleto = 0;
+        
+        int valorEmpresa = 0;
+        
+        int numVenda = 0;
+        
+        byte cod;
+        char tipo;
+        char extraIngr;
+        float valor;
         
         do {
             System.out.println("1. Receber os dados da Venda");
